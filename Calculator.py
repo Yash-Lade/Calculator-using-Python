3b8d07042133ecf57ccfbfdba7dcbf5ed1548971
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-11-10 16:43:34 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+a=int(input("Enter the value of a :"))
+b=int(input("Enter the value of b :"))
+add=a+b
+mul=a*b
+subs=a-b
+div=a/b
+mod=a%b
+exp=a**b
+flodiv=a//b # it gives the value of divison but before of decimal !!
+
+print("The addition of ",a,"and",b,"is",add)
+print("The substraction of ",a,"and",b,"is",subs)
+print("The multiplication of ",a,"and",b,"is",mul)
+print("The division of ",a,"and",b,"is",div)
+print("The modulus of ",a,"and",b,"is",mod)
+print("The exponent of ",a,"and",b,"is",exp)
+print("The floor division of ",a,"and",b,"is",flodiv)
